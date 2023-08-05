451bf6cfd1f1d2fc6604bfb8e144eef2b04efb97
TuanThoNgay9x <skylove4298@gmail.com>
2023-08-05 19:03:39 +0700

    Add project files.

---
+++ DataLoggerAppV1/MainForm.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace DataLoggerAppV1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textUserName = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnSettings = new System.Windows.Forms.Button();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.btnAlarmList = new System.Windows.Forms.Button();
+            this.btnDashboard = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.lblNameOfPage = new System.Windows.Forms.Label();
+            this.lblSystemName = new System.Windows.Forms.Label();
+            this.pnlFormLoader = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.btnSettings);
+            this.panel1.Controls.Add(this.btnExport);
+            this.panel1.Controls.Add(this.btnAlarmList);
+            this.panel1.Controls.Add(this.btnDashboard);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(193, 1080);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.textUserName);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(187, 345);
+            this.panel2.TabIndex = 1;
+            // 
+            // textUserName
+            // 
+            this.textUserName.Location = new System.Drawing.Point(0, 197);
+            this.textUserName.Name = "textUserName";
+            this.textUserName.Size = new System.Drawing.Size(187, 24);
+            this.textUserName.TabIndex = 1;
+            this.textUserName.Text = "User Name";
+            this.textUserName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.textUserName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 56);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(190, 138);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.FlatAppearance.BorderSize = 0;
+            this.btnSettings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSettings.Image = ((System.Drawing.Image)(resources.GetObject("btnSettings.Image")));
+            this.btnSettings.Location = new System.Drawing.Point(0, 852);
+            this.btnSettings.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Size = new System.Drawing.Size(193, 73);
+            this.btnSettings.TabIndex = 1;
+            this.btnSettings.Text = "Settings";
+            this.btnSettings.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnSettings.UseVisualStyleBackColor = true;
+            this.btnSettings.Click += new System.EventHandler(this.btnSettings_Click);
+            this.btnSettings.Leave += new System.EventHandler(this.btnSettings_Leave);
+            // 
+            // btnExport
+            // 
+            this.btnExport.FlatAppearance.BorderSize = 0;
+            this.btnExport.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExport.Image = ((System.Drawing.Image)(resources.GetObject("btnExport.Image")));
+            this.btnExport.Location = new System.Drawing.Point(0, 717);
+            this.btnExport.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(193, 73);
+            this.btnExport.TabIndex = 1;
+            this.btnExport.Text = "Export";
+            this.btnExport.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.button3_Click);
+            this.btnExport.Leave += new System.EventHandler(this.btnExport_Leave);
+            // 
+            // btnAlarmList
+            // 
+            this.btnAlarmList.FlatAppearance.BorderSize = 0;
+            this.btnAlarmList.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAlarmList.Image = ((System.Drawing.Image)(resources.GetObject("btnAlarmList.Image")));
+            this.btnAlarmList.Location = new System.Drawing.Point(0, 582);
+            this.btnAlarmList.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.btnAlarmList.Name = "btnAlarmList";
+            this.btnAlarmList.Size = new System.Drawing.Size(193, 73);
+            this.btnAlarmList.TabIndex = 1;
+            this.btnAlarmList.Text = "Alarm List";
+            this.btnAlarmList.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnAlarmList.UseVisualStyleBackColor = true;
+            this.btnAlarmList.Click += new System.EventHandler(this.btnAlarmList_Click);
+            this.btnAlarmList.Leave += new System.EventHandler(this.btnAlarmList_Leave);
+            // 
+            // btnDashboard
+            // 
+            this.btnDashboard.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnDashboard.FlatAppearance.BorderSize = 0;
+            this.btnDashboard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDashboard.Image = ((System.Drawing.Image)(resources.GetObject("btnDashboard.Image")));
+            this.btnDashboard.Location = new System.Drawing.Point(0, 448);
+            this.btnDashboard.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.btnDashboard.Name = "btnDashboard";
+            this.btnDashboard.Size = new System.Drawing.Size(193, 73);
+            this.btnDashboard.TabIndex = 1;
+            this.btnDashboard.Text = "Dashboard";
+            this.btnDashboard.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnDashboard.UseVisualStyleBackColor = false;
+            this.btnDashboard.Click += new System.EventHandler(this.btnDashboard_Click);
+            this.btnDashboard.Leave += new System.EventHandler(this.btnDashboard_Leave);
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.lblNameOfPage);
+            this.panel3.Controls.Add(this.lblSystemName);
+            this.panel3.Location = new System.Drawing.Point(183, -8);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1760, 90);
+            this.panel3.TabIndex = 1;
+            // 
+            // lblNameOfPage
+            // 
+            this.lblNameOfPage.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNameOfPage.Location = new System.Drawing.Point(1601, 64);
+            this.lblNameOfPage.Name = "lblNameOfPage";
+            this.lblNameOfPage.Size = new System.Drawing.Size(135, 22);
+            this.lblNameOfPage.TabIndex = 1;
+            this.lblNameOfPage.Text = "Page Name";
+            this.lblNameOfPage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblNameOfPage.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblSystemName
+            // 
+            this.lblSystemName.Font = new System.Drawing.Font("Times New Roman", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSystemName.Location = new System.Drawing.Point(571, 8);
+            this.lblSystemName.Name = "lblSystemName";
+            this.lblSystemName.Size = new System.Drawing.Size(630, 82);
+            this.lblSystemName.TabIndex = 1;
+            this.lblSystemName.Text = "Data Logger CDQC System";
+            this.lblSystemName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblSystemName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pnlFormLoader
+            // 
+            this.pnlFormLoader.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlFormLoader.Location = new System.Drawing.Point(193, 86);
+            this.pnlFormLoader.Name = "pnlFormLoader";
+            this.pnlFormLoader.Size = new System.Drawing.Size(1727, 994);
+            this.pnlFormLoader.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1920, 1080);
+            this.ControlBox = false;
+            this.Controls.Add(this.pnlFormLoader);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel3);
+            this.Font = new System.Drawing.Font("Times New Roman", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnDashboard;
+        private System.Windows.Forms.Button btnSettings;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Button btnAlarmList;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label textUserName;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label lblNameOfPage;
+        private System.Windows.Forms.Label lblSystemName;
+        private System.Windows.Forms.Panel pnlFormLoader;
+    }
+}
+
---
+++ DataLoggerAppV1/MainForm.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace DataLoggerAppV1
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            btnDashboard.BackColor = SystemColors.Control;
+
+            lblNameOfPage.Text = "Dashboard";
+            this.pnlFormLoader.Controls.Clear();
+            Dashboard Dashboard_Vrb = new Dashboard() { Dock = DockStyle.Fill, TopLevel = false, TopMost = true };
+            Dashboard_Vrb.FormBorderStyle = FormBorderStyle.None;
+            this.pnlFormLoader.Controls.Add(Dashboard_Vrb);
+            Dashboard_Vrb.Show();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            lblNameOfPage.Text = "Export";
+            this.pnlFormLoader.Controls.Clear();
+            Export Export_Vrb = new Export() { Dock = DockStyle.Fill, TopLevel = false, TopMost = true };
+            Export_Vrb.FormBorderStyle = FormBorderStyle.None;
+            this.pnlFormLoader.Controls.Add(Export_Vrb);
+            Export_Vrb.Show();
+
+            btnDashboard.BackColor = SystemColors.ControlLight;
+            btnAlarmList.BackColor = SystemColors.ControlLight;
+            btnExport.BackColor = SystemColors.Control;
+            btnSettings.BackColor = SystemColors.ControlLight;
+        }
+
+        private void btnExport_Leave(object sender, EventArgs e)
+        {
+            
+        }
+
+
+
+        private void btnDashboard_Click(object sender, EventArgs e)
+        {
+            lblNameOfPage.Text = "Dashboard";
+            this.pnlFormLoader.Controls.Clear();
+            Dashboard Dashboard_Vrb = new Dashboard() { Dock = DockStyle.Fill, TopLevel = false, TopMost = true };
+            Dashboard_Vrb.FormBorderStyle = FormBorderStyle.None;
+            this.pnlFormLoader.Controls.Add(Dashboard_Vrb);
+            Dashboard_Vrb.Show();
+
+            btnDashboard.BackColor = SystemColors.Control;
+            btnAlarmList.BackColor = SystemColors.ControlLight;
+            btnExport.BackColor = SystemColors.ControlLight;
+            btnSettings.BackColor = SystemColors.ControlLight;
+        }
+
+        private void btnDashboard_Leave(object sender, EventArgs e)
+        {
+            
+        }
+
+
+
+        private void btnAlarmList_Click(object sender, EventArgs e)
+        {
+            lblNameOfPage.Text = "Alarm List";
+            this.pnlFormLoader.Controls.Clear();
+            Alarm Alarm_Vrb = new Alarm() { Dock = DockStyle.Fill, TopLevel = false, TopMost = true };
+            Alarm_Vrb.FormBorderStyle = FormBorderStyle.None;
+            this.pnlFormLoader.Controls.Add(Alarm_Vrb);
+            Alarm_Vrb.Show();
+
+            btnDashboard.BackColor = SystemColors.ControlLight;
+            btnAlarmList.BackColor = SystemColors.Control;
+            btnExport.BackColor = SystemColors.ControlLight;
+            btnSettings.BackColor = SystemColors.ControlLight;
+        }
+
+        private void btnAlarmList_Leave(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void btnSettings_Click(object sender, EventArgs e)
+        {
+            lblNameOfPage.Text = "Settings";
+            this.pnlFormLoader.Controls.Clear();
+            Setting Setting_Vrb = new Setting() { Dock = DockStyle.Fill, TopLevel = false, TopMost = true };
+            Setting_Vrb.FormBorderStyle = FormBorderStyle.None;
+            this.pnlFormLoader.Controls.Add(Setting_Vrb);
+            Setting_Vrb.Show();
+
+            btnDashboard.BackColor = SystemColors.ControlLight;
+            btnAlarmList.BackColor = SystemColors.ControlLight;
+            btnExport.BackColor = SystemColors.ControlLight;
+            btnSettings.BackColor = SystemColors.Control;
+        }
+
+        private void btnSettings_Leave(object sender, EventArgs e)
+        {
+            
+        }
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
