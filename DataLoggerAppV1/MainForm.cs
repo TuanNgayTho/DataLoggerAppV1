d51011ea6436064fc7bf44eeadf6eb4e7421ff3a
TuanNgayTho <thienthancuto@gmail.com>
2023-08-18 12:52:43 +0700

    18/08/2023

--- DataLoggerAppV1/MainForm.cs
+++ DataLoggerAppV1/MainForm.cs
@@ -19,6 +19,14 @@ namespace DataLoggerAppV1
         public static bool IsLogIn = false;
         public Label UserNameLabelel;
         public static Plc plc = new Plc(CpuType.S71200, "192.168.0.2", 0, 1);
+        public double DataAi0;
+        public double DataAi1;
+        public double DataAi2;
+        public double DataAi3;
+        public double DataAi4;
+        public double DataAi5;
+        public double DataAi6;
+        public double DataAi7;
         public MainForm()
         {
             InitializeComponent();
@@ -41,11 +49,23 @@ namespace DataLoggerAppV1
             });
             t.IsBackground = true;
             t.Start();
+
+            Thread t2 = new Thread(() =>
+            {
+                WriteAlarmToDatabase();
+            });
+            t2.IsBackground = true;
+            t2.Start();
         }
-        private void WriteSampleToDatabase()
+
+        private void WriteAlarmToDatabase()
         {
+            bool sttLowAlarm = false;
+            bool sttHighAlarm = false;
+
             while (true)
             {
+                Thread.Sleep(100);
                 var result = plc.Open();
                 while (true)
                 {
@@ -65,28 +85,48 @@ namespace DataLoggerAppV1
                             var DbAiData = new DbAiData();
                             plc.ReadClass(DbAiData, 4);
 
+                            // Read Bool Status Data From PLC
+                            var DbReadBool = new DbReadBool();
+                            plc.ReadClass(DbReadBool, 13);
+
+                            DataAi0 = DbAiData.Ai0;
+                            DataAi1 = DbAiData.Ai1;
+                            DataAi2 = DbAiData.Ai2;
+                            DataAi3 = DbAiData.Ai3;
+                            DataAi4 = DbAiData.Ai4;
+                            DataAi5 = DbAiData.Ai5;
+                            DataAi6 = DbAiData.Ai6;
+                            DataAi7 = DbAiData.Ai7;
+
                             //Connect To Mysql
                             string server = "localhost";
                             string database = "datalogger";
                             string uid = "root";
                             string password = "root";
                             string constring = "Server=" + server + "; database=" + database + "; uid=" + uid + "; pwd=" + password;
-                            using (MySqlConnection con = new MySqlConnection(constring))
+
+                            using (MySqlConnection con2 = new MySqlConnection(constring))
                             {
-                                con.Open();
-                                var query = "INSERT INTO samples (aivalue0, aivalue1, aivalue2, aivalue3, aivalue4, aivalue5, aivalue6, aivalue7) " +
-                                            "VALUES (" + DbAiData.Ai0 + ", " + DbAiData.Ai1 + ", "+DbAiData.Ai2+"," + DbAiData.Ai3 + ", " + DbAiData.Ai4 + ", "+DbAiData.Ai5+", " + DbAiData.Ai6 + ", "+DbAiData.Ai7+");";
-                                StringBuilder bu = new StringBuilder();
-                                MySqlCommand cmd = new MySqlCommand(query, con);
-                                try
+                                con2.Open();
+                                // Ai0 write alarm to database
+                                if (DbReadBool.LowAlarm0 == true && sttLowAlarm == true)
                                 {
-                                    if (cmd.ExecuteNonQuery() == 1)
+                                    var query = "INSERT INTO alarmlist (ainame, aivalue0, aivalue1, aivalue2, aivalue3, aivalue4, aivalue5, aivalue6, aivalue7) " +
+                                                "VALUES ('" + Properties.Settings.Default.NameAi0 + ", " + DataAi0 + ", " + DataAi1 + ", " + DataAi2 + ", " + DataAi3 + ", " + DataAi4 + ", " + DataAi5 + ", " + DataAi6 + ", " + DataAi7 + ");";
+                                    StringBuilder bu = new StringBuilder();
+                                    MySqlCommand cmd = new MySqlCommand(query, con2);
+                                    try
                                     {
+                                        if (cmd.ExecuteNonQuery() == 1)
+                                        {
+                                        }
+                                    }
+                                    catch (Exception e)
+                                    {
+                                        MessageBox.Show(e.Message);
                                     }
                                 }
-                                catch (Exception e)
-                                {
-                                }
+                                con2.Close();
                             }
                         }
                     }
@@ -100,6 +140,48 @@ namespace DataLoggerAppV1
                 }
             }
         }
+
+        private void WriteSampleToDatabase()
+        {
+            while (true)
+            {
+                try
+                {
+                    //Connect To Mysql
+                    Thread.Sleep(Properties.Settings.Default.CycleTime);
+                    string server = "localhost";
+                    string database = "datalogger";
+                    string uid = "root";
+                    string password = "root";
+                    string constring = "Server=" + server + "; database=" + database + "; uid=" + uid + "; pwd=" + password;
+                    using (MySqlConnection con = new MySqlConnection(constring))
+                    {
+                        con.Open();
+                        var query = "INSERT INTO samples (aivalue0, aivalue1, aivalue2, aivalue3, aivalue4, aivalue5, aivalue6, aivalue7) " +
+                                    "VALUES (" + DataAi0 + ", " + DataAi1 + ", "+ DataAi2 + "," + DataAi3 + ", " + DataAi4 + ", "+ DataAi5 + ", " + DataAi6 + ", "+ DataAi7 +");";
+                        StringBuilder bu = new StringBuilder();
+                        MySqlCommand cmd = new MySqlCommand(query, con);
+                        try
+                        {
+                            if (cmd.ExecuteNonQuery() == 1)
+                            {
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                        }
+                        con.Close();
+                    }
+                    
+                }
+                catch (Exception e)
+                {
+                    Thread.Sleep(100);
+                }
+            }
+            
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
 
