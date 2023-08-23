65942dc7c1e6f6938339bc6a38e59cb85714fc85
PC <PC@DESKTOP-IDI1BPR>
2023-08-23 10:50:15 +0700

    hjgjjgj

--- DataLoggerAppV1/Trend.Designer.cs
+++ DataLoggerAppV1/Trend.Designer.cs
@@ -28,21 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Export
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(388, 99);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Trend
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.PaleGreen;
             this.ClientSize = new System.Drawing.Size(1712, 1002);
+            this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "Export";
+            this.Name = "Trend";
             this.Text = "Export";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
