0729983a86391d27933a0f6884b2dddc1cabfbe9
TuanNgayTho <thienthancuto@gmail.com>
2023-08-25 11:35:20 +0700

    updATE

--- DataLoggerAppV1/MainForm.cs
+++ DataLoggerAppV1/MainForm.cs
@@ -101,7 +101,6 @@ namespace DataLoggerAppV1
                     {
                         if (result != ErrorCode.NoError)
                         {
-                           
                             MessageBox.Show("Error: abc" + plc.LastErrorCode + "\n" + plc.LastErrorString);
                             break;
                         }
