01a693eadce6d282940ebef344612c8b6efec673
PC <PC@DESKTOP-IDI1BPR>
2023-08-25 10:57:48 +0700

    abc

--- DataLoggerAppV1/MainForm.cs
+++ DataLoggerAppV1/MainForm.cs
@@ -36,6 +36,7 @@ namespace DataLoggerAppV1
         public static bool boolBypassStream1;
         public static bool boolBypassStream2;
         public static bool boolBypassStream3;
+        public static bool isConnect;
         
 
         public MainForm()
