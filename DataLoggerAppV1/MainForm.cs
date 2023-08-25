88e690c25f4aa851410420a75b2435f3e42bab44
TuanNgayTho <thienthancuto@gmail.com>
2023-08-25 11:32:44 +0700

    delete Isconnect

--- DataLoggerAppV1/MainForm.cs
+++ DataLoggerAppV1/MainForm.cs
@@ -94,18 +94,8 @@ namespace DataLoggerAppV1
             {
                 Thread.Sleep(100);
                 var result = plc.Open();
-                if (result != ErrorCode.NoError)
-                {
-                    isConnect = false;
-                    MessageBox.Show(Convert.ToString(isConnect));
-           
-                }
-                else
-                {
-                    isConnect = false;
-                    MessageBox.Show(Convert.ToString(isConnect));
-                }
-                    while (true)
+
+                while (true)
                 {
                     try
                     {
@@ -113,13 +103,10 @@ namespace DataLoggerAppV1
                         {
                            
                             MessageBox.Show("Error: abc" + plc.LastErrorCode + "\n" + plc.LastErrorString);
-                            MessageBox.Show(Convert.ToString(isConnect));
-                            isConnect = false;
                             break;
                         }
                         else
                         {
-                            
                             // Read AI Data From PLC
                             var DbAiData = new DbAiData();
                             plc.ReadClass(DbAiData, 4);
