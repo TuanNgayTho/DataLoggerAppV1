216c833b9143e6269a65b8fe4585e17842807b0e
TuanNgayTho <thienthancuto@gmail.com>
2023-09-18 14:30:58 +0700

    18/09/2023

--- DataLoggerAppV1/Dashboard.Designer.cs
+++ DataLoggerAppV1/Dashboard.Designer.cs
@@ -861,7 +861,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(101, 81);
             this.label1.TabIndex = 8;
-            this.label1.Text = "Name";
+            this.label1.Text = "Parameter";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
